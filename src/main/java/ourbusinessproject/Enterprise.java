022e826653845e91bc169dda08e473d65827deaa
Franck Silvestre <franck.silvestre@gmail.com>
2024-04-03 11:04:06 +0200

    fix #1.2 classe Enterprise

---
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -0,0 +1,80 @@
+package ourbusinessproject;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class Enterprise {
+    @NotEmpty
+    private String name;
+    @Size(min = 10)
+    private String description;
+    @NotEmpty
+    private String contactName;
+    @NotEmpty @Email
+    private String contactEmail;
+
+    /**
+     * set the name
+     * @param name the new name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get the name
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the description
+     * @param description the new description
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Get the description
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Set the contact name
+     * @param contactName the new contact name
+     */
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    /**
+     * Get the contact name
+     * @return the contact name
+     */
+    public String getContactName() {
+        return contactName;
+    }
+
+    /**
+     * Set the contact email
+     * @param contactEmail the new contact email
+     */
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    /**
+     * Get the contact email
+     * @return the contact email
+     */
+    public String getContactEmail() {
+        return contactEmail;
+    }
+}
