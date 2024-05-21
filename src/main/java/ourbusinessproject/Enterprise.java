6b1c4b6c0c017aae622626fc71aea0906377f31e
Franck Silvestre <franck.silvestre@gmail.com>
2024-05-21 15:24:51 +0200

    fix #2.2 Relation enterprise project

--- src/main/java/ourbusinessproject/Enterprise.java
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -2,95 +2,119 @@ package ourbusinessproject;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ * An enterprise is a company that is a project partner.
+ *
+ */
 @Entity
 public class Enterprise {
-    @NotEmpty
-    private String name;
-    @Size(min = 10)
-    private String description;
-    @NotEmpty
-    private String contactName;
+    @NotEmpty private String name;
+    @Size(min = 10) private String description;
+    @NotEmpty private String contactName;
     @NotEmpty @Email
     private String contactEmail;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue
     private Long id;
+    @OneToMany(mappedBy = "enterprise")
+    private Collection<Project> projects;
 
     /**
-     * set the name
-     * @param name the new name
+     * Set the name of the enterprise
+     * @param name the name of the enterprise
      */
     public void setName(String name) {
         this.name = name;
     }
 
     /**
-     * Get the name
-     * @return the name
+     * Get the name of the enterprise
+     * @return the name of the enterprise
      */
     public String getName() {
         return name;
     }
 
     /**
-     * Set the description
-     * @param description the new description
+     * Set the description of the enterprise
+     * @param description the description of the enterprise
      */
     public void setDescription(String description) {
         this.description = description;
     }
 
     /**
-     * Get the description
-     * @return the description
+     * Get the description of the enterprise
+     * @return the description of the enterprise
      */
     public String getDescription() {
         return description;
     }
 
     /**
-     * Set the contact name
-     * @param contactName the new contact name
+     * Set the contact name of the enterprise
+     * @param contactName the contact name of the enterprise
      */
     public void setContactName(String contactName) {
         this.contactName = contactName;
     }
 
     /**
-     * Get the contact name
-     * @return the contact name
+     * Get the contact name of the enterprise
+     * @return the contact name of the enterprise
      */
     public String getContactName() {
         return contactName;
     }
 
     /**
-     * Set the contact email
-     * @param contactEmail the new contact email
+     * Set the contact email of the enterprise
+     * @param contactEmail the contact email of the enterprise
      */
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
     }
 
     /**
-     * Get the contact email
-     * @return the contact email
+     * Get the contact email of the enterprise
+     * @return the contact email of the enterprise
      */
     public String getContactEmail() {
         return contactEmail;
     }
 
     /**
-     * Get the id
-     * @return the id
+     * Get the id of the enterprise
+     * @return the id of the enterprise
      */
     public Long getId() {
         return id;
+    }
+
+    /**
+     * Get the projects of the enterprise
+     * @return the projects of the enterprise
+     */
+    public Collection<Project> getProjects() {
+        return projects;
+    }
+
+    /**
+     * Add a project to the enterprise
+     * @param project the project to add
+     */
+    public void addProject(Project project) {
+        if (projects == null)
+            projects = new HashSet<>();
+        this.projects.add(project);
     }
 }
